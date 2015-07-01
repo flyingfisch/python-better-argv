8a281e3a3005992d4b58c9c4673939485c6f1b3d
flyingfisch <flyingfisch@toppagedesign.com>
2015-06-30 20:10:32 -0400

    finished args()

--- betterargv.py
+++ betterargv.py
@@ -1,6 +1,24 @@
-import argv
+#!/usr/bin/python3
+
+import sys
 
 def args():
-    
+    args = []
+    argv = sys.argv
 
-    return #list of strings
+    for counter, arg in enumerate(argv):
+        if arg[:1] != '-' :
+            # if this arg is a value, skip it
+            continue
+        elif (counter < len(argv) - 2) and (argv[counter + 1][:1] != '-'):
+            # if this arg has a value, return a tuple of (arg, value)
+            newArg = (arg, argv[counter + 1])
+        else:
+            # if this is a flag, return it by itself
+            newArg = (arg, '')
+
+        args.append(newArg)
+
+    return args
+
+print(args())
